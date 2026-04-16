b2d13576d4c604683f2e02154fb87118d3470ecd
LaShaylaM35 <lashaylam@yahoo.com>
2026-04-15 20:41:11 -0500

    second commit

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Persons (
+    ID int PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50),
+    address varchar(100),
+    Age int
+);
+
+select * from Persons
